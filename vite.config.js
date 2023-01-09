6d5b3ff00bbee366549da5963915432fccc385e2
wjoseph0 <wjoseph0@proton.me>
2023-01-09 14:08:28 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login/index.html'),
+        app: resolve(__dirname, 'app/index.html'),
+      },
+    },
+  },
+})
