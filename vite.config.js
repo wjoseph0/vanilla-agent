867a57c6f4868ed7f8aecbbb959934d37b297c26
wjoseph0 <wjoseph0@proton.me>
2023-01-09 18:12:51 -0600

    docs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   root: 'src',
   build: {
     emptyOutDir: true,
-    outDir: '../dist',
+    outDir: '../docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
