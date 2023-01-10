1bc4c271415ffcc5634e4ae8d44209fc0af6604e
wjoseph0 <wjoseph0@proton.me>
2023-01-09 22:15:42 -0600

    remove icon

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   root: 'src',
   build: {
     emptyOutDir: true,
-    outDir: '../docs',
+    outDir: '../dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
