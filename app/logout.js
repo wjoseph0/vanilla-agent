7a7ab5e60d80283add87d897ca8f23dfc9a4ba82
wjoseph0 <wjoseph0@proton.me>
2023-01-10 13:46:34 -0600

    comment styling

--- app/logout.js
+++ app/logout.js
@@ -1,17 +1,17 @@
-//import pocketbase
+// Import pocketbase
 import { pb } from '../pocketbase.js';
 
-//Show user status message
+// Show user status message
 const userStatusContainer = document.getElementById('userStatusContainer');
 const status = document.createElement('p');
 status.textContent = `Signed in as: ${pb.authStore.model.username}`;
 userStatusContainer.append(status);
 
-//Allow user to logout
+// Allow user to logout
 const logoutButton = document.createElement('button');
 logoutButton.textContent = 'Logout';
 logoutButton.addEventListener('click', () => {
-  pb.collection('messages').unsubscribe(); //end realtime connection
+  pb.collection('messages').unsubscribe(); // End realtime connection
   pb.authStore.clear();
   location.replace('/login/');
 });
