5fe54cd3ab9caf5413b9ba4447b1fcd2e76458e2
wjoseph0 <wjoseph0@proton.me>
2023-01-10 01:54:41 -0600

    messages feature prototype

--- app/logout.js
+++ app/logout.js
@@ -2,16 +2,17 @@
 import { pb } from '../pocketbase.js';
 
 //Show user status message
-const container = document.getElementById('container');
+const userStatusContainer = document.getElementById('userStatusContainer');
 const status = document.createElement('p');
 status.textContent = `Signed in as: ${pb.authStore.model.username}`;
-container.append(status);
+userStatusContainer.append(status);
 
 //Allow user to logout
 const logoutButton = document.createElement('button');
 logoutButton.textContent = 'Logout';
 logoutButton.addEventListener('click', () => {
+  pb.collection('messages').unsubscribe(); //end realtime connection
   pb.authStore.clear();
   location.replace('/login/');
 });
-container.append(logoutButton);
+userStatusContainer.append(logoutButton);
--- app/messages.js
+++ app/messages.js
@@ -1,5 +1,69 @@
+// Import pocketbase
 import { pb } from '../pocketbase.js';
 
-pb.collection('messages').subscribe('*', (e) => {
-  console.log(e.record);
+// Grab messagesContainer for reference
+const messagesContainer = document.getElementById('messagesContainer');
+
+// Declare global messages var
+let messages;
+
+// Get initial messages
+const resultList = await pb.collection('messages').getList(1, 50, {
+  sort: 'created',
+});
+messages = resultList.items;
+
+// Push messages to DOM
+function showMessages() {
+  for (let index = 0; index < messages.length; index++) {
+    const message = messages[index];
+    const p = document.createElement('p');
+    p.textContent = message.text;
+    messagesContainer.append(p);
+  }
+}
+// Initial load: Push messages to DOM
+showMessages();
+
+// Clear all children of element
+function removeChilds(parent) {
+  while (parent.lastChild) {
+    parent.removeChild(parent.lastChild);
+  }
+};
+
+// Subscribe to realtime messages
+await pb.collection('messages').subscribe('*', async ({ action, record }) => {
+  removeChilds(messagesContainer); // Clear messages
+
+  if (action === 'create') {
+    const user = await pb.collection('users').getOne(record.user); // Fetch associated user
+    record.expand = { user };
+    messages = [...messages, record]; // Update messages
+    showMessages(); // Push messages to DOM
+  }
+  if (action === 'delete') {
+    messages = messages.filter((m) => m.id !== record.id); // Update messages
+    showMessages(); // Push messages to DOM
+  }
+});
+
+// Grab new message form elements
+const newMessageForm = document.getElementById('newMessageForm');
+const messageInput = document.getElementById('messageInput');
+
+// Send new message
+async function sendMessage() {
+  const data = {
+    text: messageInput.value,
+    user: pb.authStore.model.id
+  };
+  await pb.collection('messages').create(data);
+  messageInput.value = '';
+}
+
+// Handle new message form submit
+newMessageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendMessage();
 });
